9434ca404d3abbaf535a92801280d1a254c357c5
arthur-er <arthur-er@protonmail.com>
2021-03-19 23:40:50 -0300

    perf(repo): setup eslint in base package instead of using a package for configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,43 @@
-const styleguide = require("@monorepo/styleguide");
-
-module.exports = styleguide
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+    jest: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+    'prettier',
+    'eslint-plugin-import-helpers'
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    'space-before-function-paren': 'off',
+    'react/prop-types': 'off',
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error'],
+    'import-helpers/order-imports': [
+      'warn',
+      {
+        newlinesBetween: 'always',
+        groups: ['module', '/^@[a-z]+/', ['parent', 'sibling', 'index']],
+        alphabetize: { order: 'asc', ignoreCase: true }
+      }
+    ]
+  }
+}
